c03bea87c51d56922cc21a7bfd53edc63c68e1ad
thmsrzk <thoms.rzk@gmail.com>
2024-08-09 22:48:43 +0200

    Installed Sass. Added SST Fonts into public/fonts and imported some in global.scss Added new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/Styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from "next";
+
+import "@/Sass/globals.scss";
+
 
 export const metadata: Metadata = {
   title: "Portfolio de Thomas",
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
