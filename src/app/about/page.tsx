8e4111ef19d4bea2f101b041661dee1d9b3f015a
Thomas <thoms.rzk@gmail.com>
2024-08-21 22:57:50 +0200

    About page now map throught about db (json for now) to display cards.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,12 @@
-
 'use client';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import aboutData from '@/src/data/about.json';
 import Card from '@/src/components/Card/Card';
+import CardDetails from '@/src/components/CardDetails/CardDetails';
 
-const About = () => {
+const Projects = () => {
   const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(null);
 
   const handleCardClick = (index: number) => {
@@ -13,35 +14,33 @@ const About = () => {
   };
 
   return (
-    <div className="min-h-full py-11 border-3 border-red-500">
+    <div className="min-h-full h-full py-11 border-3 border-red-500 flex flex-col">
       <section className='flex gap-4'>
-        <Card
-          card={aboutData[0]}
-          isSelected={selectedCardIndex === 0}
-          onClick={() => handleCardClick(0)}
-          disableHover={selectedCardIndex !== null}
-        />
-        <Card
-          card={aboutData[0]}
-          isSelected={selectedCardIndex === 0}
-          onClick={() => handleCardClick(0)}
-          disableHover={selectedCardIndex !== null}
-        />
-        <Card
-          card={aboutData[0]}
-          isSelected={selectedCardIndex === 0}
-          onClick={() => handleCardClick(0)}
-          disableHover={selectedCardIndex !== null}
-        />
-        <Card
-          card={aboutData[0]}
-          isSelected={selectedCardIndex === 0}
-          onClick={() => handleCardClick(0)}
-          disableHover={selectedCardIndex !== null}
-        />
+        {aboutData.map((project, index) => (
+          <Card
+            key={index}
+            card={project}
+            isSelected={selectedCardIndex === index}
+            onClick={() => handleCardClick(index)}
+            disableHover={selectedCardIndex !== null}
+          />
+        ))}
       </section>
+      <AnimatePresence>
+        {selectedCardIndex !== null && (
+          <motion.div
+            initial={{ width: 0, height: 0, opacity: 0 }}
+            animate={{ width: '100%', height: '100%', opacity: 1 }}
+            exit={{ width: 0, height: 0, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="overflow-hidden bg-white flex-grow mt-4 rounded-2xl p-4"
+          >
+            <CardDetails card={aboutData[selectedCardIndex]} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
-export default About;
+export default Projects;
