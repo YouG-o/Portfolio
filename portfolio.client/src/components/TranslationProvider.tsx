9788a127d321402e404f8ebe4a1811f0b26ae3b3
Thomas <thoms.rzk@gmail.com>
2024-09-21 23:36:49 +0200

    Client :
    - Added an interface for the props of the TranslationsProvider component.
    - Typed the props to ensure type safety and better developer experience.

--- portfolio.client/src/components/TranslationProvider.tsx
+++ portfolio.client/src/components/TranslationProvider.tsx
@@ -1,15 +1,23 @@
 'use client';
 
+import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import initTranslations from '@/src/i18n';
-import { createInstance } from 'i18next';
+import { createInstance, Resource, InitOptions } from 'i18next';
+
+interface TranslationsProviderProps {
+  children: ReactNode;
+  locale: string;
+  namespaces: string[];
+  resources: Resource;
+}
 
 export default function TranslationsProvider({
   children,
   locale,
   namespaces,
   resources
-}) {
+}: TranslationsProviderProps) {
   const i18n = createInstance();
 
   initTranslations(locale, namespaces, i18n, resources);
