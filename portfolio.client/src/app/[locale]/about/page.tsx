747e5f73055796ed288f2b09a6cb7a9d8b4383b4
Thomas <thoms.rzk@gmail.com>
2024-09-22 10:19:31 +0200

    Client : About page data is now prefetched on Homepage and stored in local storage

--- portfolio.client/src/app/[locale]/about/page.tsx
+++ portfolio.client/src/app/[locale]/about/page.tsx
@@ -2,27 +2,36 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { motion, AnimatePresence } from 'framer-motion';
 
+import { motion, AnimatePresence } from 'framer-motion';
 import { WhoAmI, Training, Contact } from '@/src/types/About';
+import { getAbout } from '@/src/utils/api';
 import Card from '@/src/components/Card';
 import CardContent from '@/src/components/CardContent/CardContent';
-import { getAbout } from '@/src/utils/api';
-
 
 const About = () => {
   const [aboutData, setAboutData] = useState<(WhoAmI | Training | Contact)[]>([]);
   const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const searchParams = useSearchParams();
   const openCard = searchParams.get('Card');
 
   useEffect(() => {
     const fetchAbout = async () => {
+      setIsLoading(true);
       try {
-        const data = await getAbout();
-        setAboutData(data);
+        const cachedData = localStorage.getItem('aboutData');
+        if (cachedData) {
+          setAboutData(JSON.parse(cachedData));
+          setIsLoading(false);
+        } else {
+          const data = await getAbout();
+          setAboutData(data);
+          setIsLoading(false);
+        }
       } catch (error) {
         console.error('Error fetching about data:', error);
+        setIsLoading(false);
       }
     };
 
@@ -38,6 +47,10 @@ const About = () => {
   const handleCardClick = (index: number) => {
     setSelectedCardIndex(selectedCardIndex === index ? null : index);
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="min-h-full sm:h-full py-11 border-3 border-red-500 flex flex-col">
--- portfolio.client/src/app/[locale]/page.tsx
+++ portfolio.client/src/app/[locale]/page.tsx
@@ -1,19 +1,35 @@
-// page.tsx
+
+'use client';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { getAbout } from '@/src/utils/api';
 import Image from "next/image";
 import PSLogo from "@/public/images/playstation_logo.png";
 import PageTransition from "@/src/components/PageTransition";
-import initTranslations from "@/src/i18n";
-import { PageParams } from "@/src/types/Types";
 
-interface HomePageProps {
-  params: PageParams;
-}
+const HomePage = () => {
+  const router = useRouter();
+  const { t } = useTranslation('HomePage');
 
-const HomePage = async ({ params: { locale } }: HomePageProps) => {
-  const { t } = await initTranslations(locale, ["HomePage"]);
+  useEffect(() => {
+    const prefetchAboutData = async () => {
+      try {
+        const aboutData = await getAbout();
+        localStorage.setItem('aboutData', JSON.stringify(aboutData));
+      } catch (error) {
+        console.error('Error prefetching about data:', error);
+      }
+    };
+
+    prefetchAboutData();
+
+    router.prefetch('/about');
+  }, [router]);
 
   return (
-    <PageTransition href="/about?Card=0" duration={30}>
+    <PageTransition href="/about?Card=0" duration={0}>
       <p className="absolute top-1/4 text-center">{t("welcomeMessage")}</p>
       <Image src={PSLogo} width={300} height={300} alt="Playstation Logo" />
     </PageTransition>
