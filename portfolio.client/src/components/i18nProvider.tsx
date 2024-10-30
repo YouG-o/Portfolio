402af663beeb820429f1b434361a131802d67e5b
Thomas <thoms.rzk@gmail.com>
2024-10-30 02:14:05 +0100

    i

--- portfolio.client/src/components/i18nProvider.tsx
+++ portfolio.client/src/components/i18nProvider.tsx
@@ -1,10 +1,8 @@
 'use client';
-import { createContext, useContext, useState, ReactNode, useMemo } from 'react';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { Resource, i18n as I18nInstance } from 'i18next';
 import initTranslations from '@/src/i18n';
-import { createInstance, Resource } from 'i18next';
-
-const instance = createInstance();
 
 interface I18nProviderProps {
   children: ReactNode;
@@ -21,17 +19,27 @@ interface LanguageContextProps {
 const LanguageContext = createContext<LanguageContextProps | undefined>(undefined);
 
 export const I18nProvider = ({ children, locale, namespaces, resources }: I18nProviderProps) => {
-  const i18n = useMemo(() => {
-    initTranslations(locale, namespaces, resources);
-    return instance;
+  const [currentLocale, setCurrentLocale] = useState(locale);
+  const [i18n, setI18n] = useState<I18nInstance | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const { i18n: i18nInstance } = await initTranslations(locale, namespaces, resources);
+      setI18n(i18nInstance);
+    };
+    init();
   }, [locale, namespaces, resources]);
 
-  const [currentLocale, setCurrentLocale] = useState(locale);
-
   const changeLanguage = (newLocale: string) => {
-    i18n.changeLanguage(newLocale);
-    setCurrentLocale(newLocale);
+    if (i18n) {
+      i18n.changeLanguage(newLocale);
+      setCurrentLocale(newLocale);
+    }
   };
+
+  if (!i18n) {
+    return null; // ou un composant de chargement
+  }
 
   return (
     <I18nextProvider i18n={i18n}>
